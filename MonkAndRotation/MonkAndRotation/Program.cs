7c176b0f04b5c560ed4c6b13631f206f63041f99
RanjithR2k8 <108274487+Ranjith2k8@users.noreply.github.com>
2022-06-27 21:03:07 +0530

    Monk and Rotation
    Monk and Rotation

---
+++ MonkAndRotation/MonkAndRotation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkAndRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine() ?? string.Empty, out var t);
+
+            while (t != 0)
+            {
+                t--;
+                var s = Console.ReadLine();
+                if (s == null)
+                    continue;
+                int.TryParse(s.Split(' ')[0], out var n);
+                int.TryParse(s.Split(' ')[1], out var k);
+
+                var arr = Console.ReadLine().Split(' ');
+
+                if (n != arr.Length)
+                    continue;
+
+                var index = n - (k % n);
+
+                for (int i = index; i < n; i++)
+                {
+                    Console.Write($"{arr[i]} ");
+                }
+
+                for (int i = 0; i < index; i++)
+                {
+                    Console.Write($"{arr[i]} ");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
